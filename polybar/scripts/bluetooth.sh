c17ded599430e1054f77936625965111d256a6f4
Moesen <gustavmoesmand@gmail.com>
2022-05-10 11:38:25 +0200

    add viz of bluetooth in polybar right

---
+++ polybar/scripts/bluetooth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# not connected
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
+then
+ echo "%{F#ffffff}"
+else
+ # connected, but no device
+ if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
+ then
+   echo ""
+ else
+   # get device alias
+   DEVICE=`echo info | bluetoothctl | grep 'Alias:' | awk -F:  '{ print $2 }'`
+   if [[ -z "${DEVICE// }" ]];
+   then
+     echo "%{F#2193ff} "
+   else
+     echo "%{F#2193ff} $DEVICE "
+   fi
+ fi
+fi
