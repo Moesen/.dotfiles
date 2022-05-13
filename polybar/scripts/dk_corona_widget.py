73b05fc45c09090854454f305a1f8a9a85c8f24b
Moesen <gustavmoesmand@gmail.com>
2022-05-13 10:07:56 +0200

    changed size of bar

--- polybar/scripts/dk_corona_widget.py
+++ polybar/scripts/dk_corona_widget.py
@@ -1,1 +1,5 @@
-https://www.sst.dk/en/english/corona-eng/status-of-the-epidemic/covid-19-updates-statistics-and-charts
+import requests
+
+
+sst_url = "https://www.sst.dk/en/english/corona-eng/status-of-the-epidemic/covid-19-updates-statistics-and-charts"
+
