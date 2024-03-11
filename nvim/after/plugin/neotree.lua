84d3395971d1f22ec6fe8d77ba9d0d2e1b6153b8
Moesen <gustavmoesmand@gmail.com>
2024-03-11 09:38:28 +0100

    Change keybinds for neotree

--- nvim/after/plugin/neotree.lua
+++ nvim/after/plugin/neotree.lua
@@ -1,2 +1,4 @@
-vim.keymap.set("n", "<C-n>", ":Neotree toggle<cr>")
+vim.keymap.set("n", "<C-n>", ":Neotree toggle <cr>", {silent=true})
+vim.keymap.set("n", "<leader>n", ":Neotree dir=%:p:h<cr>")
+
 
