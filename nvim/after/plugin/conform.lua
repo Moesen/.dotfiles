47de44f669c30fb74ee6516a075ce19f9e3effcc
Moesen <gustavmoesmand@gmail.com>
2024-04-03 08:43:17 +0200

    Change line length to 100 in conform/black

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -2,9 +2,36 @@ local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "isort", "black" },
+		python = function(bufnr)
+			if require("conform").get_formatter_info("ruff_format", bufnr).available then
+				return { "ruff_format" }
+			else
+				return { "isort", "black" }
+			end
+		end,
 		javascript = { { "prettierd", "prettier" } },
 		json = { "jq" },
+		["_"] = { "trim_whitespace" },
+	},
+	format_on_save = {
+		-- I recommend these options. See :help conform.format for details.
+		lsp_fallback = true,
+		timeout_ms = 500,
+	},
+	format_after_save = {
+		lsp_fallback = true,
+	},
+	formatters = {
+		black = {
+			args = {
+				"--stdin-filename",
+				"$FILENAME",
+				"--quiet",
+				"--line-length",
+				"100",
+				"-",
+			},
+		},
 	},
 })
 
